e13ebe95b1607ce5131fb82ad32e4885c5802d27
Andy Farmer <akf0676@sky.com>
2018-01-06 16:44:30 +0000

    Chapter 02 - JSX - Splitting Data and Application away from within HTML file

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
 var App = React.createClass({
-    render : function() {
-      return(React.createElement("div" , null , "Welcome"));
-    }
-});
+  render: function() {
+      // Headings data passed to Render Function from the ReactDOM.render props
+      var headings = this.props.headings.map(function(heading){
+          return <th>{heading}</th>
+      });
+      // Loop through Data passed to Render Function from the ReactDOM.render props
+      var rows = this.props.data.map(function(row){
+          return  <tr>
+                      <td>{row.when}</td>
+                      <td>{row.who}</td>
+                      <td>{row.description}</td>
+                  </tr>
+      });
+    
+      // Of important note - until React 16 - you can only return one node!!!
+      return (
+          <div>
+              <h1>{title}</h1>
+              <table>
+                  <thead>
+                      <tr>
+                          {headings}
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {rows}
+                  </tbody>
+              </table>
+          </div>
+      )
+  }
+})
 
-React.render(React.createElement(App), document.body);
+ReactDOM.render(<App title={title} headings = {headings} data = {data}/>,  document.querySelector('#App'))
---
+++ AppData.js
@@ -0,0 +1,15 @@
+var data = [{
+    "when": "2 minutes ago",
+    "who": "Jill Dupre",
+    "description": "Created new account"
+}, {
+    "when": "1 hour ago",
+    "who": "Lose White",
+    "description": "Added fist chapter"
+}, {
+    "when": "2 hours ago",
+    "who": "Jordan Whash",
+    "description": "Created new account"
+}];
+var headings = ["Last change at", "By Author", "Summary"];
+var title = 'Recent Changes';
