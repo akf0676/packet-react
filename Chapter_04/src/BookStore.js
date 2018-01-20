f06111c90a40fdbd232daa7330b1e72a78dc5fc4
Andy Farmer <akf0676@sky.com>
2018-01-20 15:25:13 +0000

    Chapter 04 - BookList Update BookList component - Utilise Arrow function - Set Intial JSON array of book data - create sub componet to render a book input field

--- Chapter_04/src/BookStore.js
+++ Chapter_04/src/BookStore.js
@@ -2,11 +2,45 @@
 import React from 'react';
 
 var BookList = React.createClass({
+    getIntialState() {
+        return (
+            {
+                books: [
+                    { name: 'Zero to One', author: 'Peter Thiel' },
+                    { name: 'Monk who sold his Ferrari', author: 'Robin Sharma' },
+                    { name: 'Wings of Fire', author: 'A.P.J. Abdul Kalam' }
+                ]
+            }
+        );
+    },
+
+    _renderBook(book) {
+        return (
+            <div class="checkbox">
+                <label for="{book.name}">
+                    <input type="checkbox" /> {book.name} -- {book.author}
+                </label>
+            </div>
+        );
+    },
     render() {
         return(
-            <h1>
-                Choose from a wider variety of books availble in our Store.
-            </h1>
+            <div>
+                <h1>
+                    Choose from a wide variety of books availble in our Store.
+                </h1>
+                <form>
+                    {/* 
+                        This is fat arrow syntax to define functions
+                        This is an ES6 feature - a shorthand for writing functions
+                        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+                    */} 
+                    {this.state.books.map((book) => {
+                        return this._renderBook(book);
+                    })}
+                    <input type="submit" className="btn btn-success" />
+                </form>
+            </div>
         );
     }
 });
