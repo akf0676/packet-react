fd6183238cfed9b96acfb123ee0b0b35d2a69e83
Andy Farmer <akf0676@sky.com>
2018-01-20 14:50:36 +0000

    Chapter 04 - BookStore Setup intial state control for the BookStore

---
+++ Chapter_04/src/BookStore.js
@@ -0,0 +1,15 @@
+//@ts-check
+import React from 'react';
+
+var BookStore = React.createClass({
+    render() {
+        switch (step) {
+            case 1:
+                return <BookList />;
+            case 2:
+                return <ShippingDetails />;
+            case 3: 
+                return <DeliveryDetails />;
+        }
+    }
+});
