de8746071b1186f85b855832a782a243fcb266b8
Andy Farmer <akf0676@sky.com>
2018-01-20 15:39:47 +0000

    Chapter 04 - Form Events -  added an onChange handler for the checkboxes (_renderBook) -  provided the value prop to the checkboxes (_renderBook) -  selectedBooks state set as an empty array in getInitialState -  new methof handleSelectedBooks --  will check whether the checkbox is checked or not via  checked prop provided by React --  Similar to value, it also gets updated with user interaction. --  At the end, we are updating the state with the new value of selectedBooks --  Therefore, at any point of time, we will have hold of selected books in this.state.selectedBooks

--- Chapter_04/src/BookStore.js
+++ Chapter_04/src/BookStore.js
@@ -9,19 +9,35 @@ var BookList = React.createClass({
                     { name: 'Zero to One', author: 'Peter Thiel' },
                     { name: 'Monk who sold his Ferrari', author: 'Robin Sharma' },
                     { name: 'Wings of Fire', author: 'A.P.J. Abdul Kalam' }
-                ]
+                ],
+                selectedBooks: []
             }
         );
     },
 
     _renderBook(book) {
         return (
-            <div class="checkbox">
-                <label for="{book.name}">
-                    <input type="checkbox" /> {book.name} -- {book.author}
+            <div className="checkbox">
+                <label htmlFor="{book.name}">
+                    <input type="checkbox" 
+                        value={book.name}
+                        onChange={this.handleSelectedBooks} /> 
+                    {book.name} -- {book.author}
                 </label>
             </div>
         );
+    },
+    handleSelectedBooks(event) {
+        var selectedBooks = this.state.selectedBooks;
+        var index = selectedBooks.indexOf(event.target.value);
+
+        if (event.target.checked) {
+            if (index === -1)
+                selectedBooks.push(event.target.value);
+        } else {
+            selectedBooks.splice(index, 1);
+        }
+        this.setState({selectedBooks : selectedBooks});
     },
     render() {
         return(
